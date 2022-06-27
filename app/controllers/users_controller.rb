d368838095b9c6e3e05e8efcdbb147d9f0e56160
Eric Araujo <ericploni@gmail.com>
2022-06-27 01:19:39 -0300

    add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class USersController > ApplicationController
+  include SuggestedUsers
+  
+  before_action :set_suggested_users, only: [:show] 
+
   def show
     user = User.find_by(username: params[:username])
     @posts = user.posts
