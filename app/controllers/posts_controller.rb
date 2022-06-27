d9afee79e4bb9799e0080998a29aa07f4ddbf6dc
Eric Araujo <ericploni@gmail.com>
2022-06-26 22:34:01 -0300

    add svgs and create  css

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        redirect_to @post, notice: "Post criado com sucesso."
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+end
